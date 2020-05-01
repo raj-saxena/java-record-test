3425cd70b9372978780f243e12a14c6437cb0114
Raj <the.raj.saxena@gmail.com>
2020-05-01 21:00:05 +0200

    first commit

---
+++ src/test/java/java/records/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package java.records.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
